780e1be9169d7731c68b5962106f802c748d73ff
Constance Anderson <vuongnhat777@gmail.com>
2024-04-27 11:45:11 +0700

    Import assets, add fruits and fruit spawner (27/04/2024).

---
+++ Fruit Slasher/Assets/Scripts/System/FruitSpawner.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour
+{
+	[Header("References"), Space]
+	[SerializeField] private GameObject[] fruitPrefabs;
+	[SerializeField] private Collider spawnArea;
+
+	[Header("Spawning Settings"), Space]
+	[SerializeField] private Vector2 spawnIntervalRange;
+	[SerializeField] private Vector2 startAngleRange;
+	[SerializeField] private Vector2 startForceRange;
+	[SerializeField] private float maxLifeTime;
+
+	// Private fields.
+	private float _interval;
+	private Bounds _bounds;
+
+	private void Start()
+	{
+		_interval = 2f;
+		_bounds = spawnArea.bounds;
+	}
+
+	private void Update()
+	{
+		SpawnFruit();
+	}
+
+	private void SpawnFruit()
+	{
+		_interval -= Time.deltaTime;
+
+		if (_interval <= 0f)
+		{
+			GameObject prefab = fruitPrefabs[Random.Range(0, fruitPrefabs.Length)];
+			
+			Vector3 position = new Vector3
+			(
+				Random.Range(_bounds.min.x, _bounds.max.x),
+				Random.Range(_bounds.min.y, _bounds.max.y),
+				Random.Range(_bounds.min.z, _bounds.max.z)
+			);
+			Quaternion rotation = Random.rotation;
+
+			Vector3 forceDirection = Quaternion.Euler(0f, 0f, Random.Range(startAngleRange.x, startAngleRange.y)) * Vector3.up;
+			Vector3 force = Random.Range(startForceRange.x, startForceRange.y) * forceDirection;
+
+			GameObject fruit = Instantiate(prefab, position, rotation);
+			fruit.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
+			Destroy(fruit, maxLifeTime);
+
+			_interval = Random.Range(spawnIntervalRange.x, spawnIntervalRange.y);
+		}
+	}
+}
